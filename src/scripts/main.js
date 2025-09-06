50cebc292ac7ac1ca76b57c8263f1117d0a32894
Zaur <zaurmamedov339933@gmail.com>
2025-09-06 18:33:48 +0300

    make build

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Uncomment the next lines to use your game instance in the browser
-import Game from '../modules/Game.class.js';
-
-const game = new Game();
-
-game.getStatus();
-// Write your code here
